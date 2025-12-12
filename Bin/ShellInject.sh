e4234d2fa484562f0e6733e74dc6019da4bddc16
Marco-Villasuso <marco.villasuso@outlook.com>
2025-12-12 14:53:24 -0700

    Add files via upload

---
+++ Bin/ShellInject.sh
@@ -0,0 +1,8 @@
+# Helicity MacOS Injector - Shell
+# Actual injection library used is lib2proc, written by Natov
+# https://www.unknowncheats.me/forum/pc-software/214802-lib2proc-definitive-os-macos-injector.html
+
+path=$(cd "$(dirname "$0")"; pwd -P)    # Get absolute path of working directory
+cd $path                                # CD to it so the relative paths work right (injector gives its own application path)
+sudo ./lib2proc $1 ./Exploit.dylib   # Inject
+# Because this is ran as sudo (super-user do); it'll ask for an admin password;
